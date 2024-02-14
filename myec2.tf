17f4e4b83a617a4702632f9b079a5a378a0d9606
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-14 19:31:20 +0530

    initial

--- myec2.tf
+++ myec2.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = "us-east-1" 
 }
 
-resource "aws_instance" "myfirst" {
+/*resource "aws_instance" "myfirst" {
     ami = "ami-0e731c8a588258d0d"
     instance_type = "t2.micro"
 
     tags ={
         Name = "test"
     }  
-}
+} */
