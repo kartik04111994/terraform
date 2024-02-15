217017104e05d05a43b04f280a1ce9bb89c0be6e
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-15 13:48:10 +0530

    updated

--- conditional.tf
+++ conditional.tf
@@ -1,14 +1,27 @@
 /*provider "aws" {
     region = "us-east-1" 
 } */
-resource "aws_instance" "dev" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
-   count = var.istest == true ? 3 : 0
+
+variable "dev" {}
+variable "prod" {
+}
+variable "istest" {
+
+}
+resource "aws_instance" "test-prod" {
+  ami           = "ami-0e731c8a588258d0d"
+  instance_type = "t2.micro"
+  count         = var.istest == true ? 2 : 0
+  tags = {
+    Name = var.prod[count.index]
+  }
 }
 
-resource "aws_instance" "prod" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.large"
-   count = var.istest == false ? 1 : 0
+resource "aws_instance" "test-dev" {
+  ami           = "ami-00d990e7e5ece7974"
+  instance_type = "t2.large"
+  count         = var.istest == false ? 2 : 0
+  tags = {
+    Name = var.dev[count.index]
+  }
 }
