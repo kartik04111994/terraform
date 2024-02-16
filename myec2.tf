a8282b84c43fd90efb53253e0c62a40e7b6738f1
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-16 13:02:04 +0530

    updating with splat expressions

--- myec2.tf
+++ myec2.tf
@@ -1,12 +1,24 @@
-/*provider "aws" {
+provider "aws" {
     region = "us-east-1" 
 }
 
-/*resource "aws_instance" "myfirst" {
+resource "aws_instance" "myfirst" {
     ami = "ami-0e731c8a588258d0d"
     instance_type = "t2.micro"
-
+    count = 2
     tags ={
-        Name = "test"
+        Name = "test.${count.index}"
     }  
-} */
+}
+
+
+# splat expression [*] for seeing all the arns of the created resource
+output "arns" {
+  value = aws_instance.myfirst[*].arn
+}
+
+# splat expression [*] for seeing all the IDs of the created resource
+# splat expression [0] for seeing IDs of first instance of the created resource
+output "arns" {
+  value = aws_instance.myfirst[*].id
+}
