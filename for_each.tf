b6497208a92888adc9b7fe33a5787cd2fd086ba3
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-16 19:51:56 +0530

    for-each

---
+++ for_each.tf
@@ -0,0 +1,22 @@
+/*resource "aws_iam_user" "demo" {
+    name = each.key
+    for_each = toset(["user1","user2","user3"])
+  
+}*/
+
+resource "aws_instance" "demoservers" {
+    ami = "ami-0e731c8a588258d0d"
+    for_each = {
+        key = "t2.micro"
+        key2 = "t2.nano"
+        
+    }
+    instance_type = each.value
+    key_name = each.key
+    tags = {
+      name = each.value
+    }
+
+    
+  
+}
