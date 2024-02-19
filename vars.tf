2be8d2c56dad449f4ad7b32324dde46d48aac4c5
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-19 22:07:31 +0530

    pushing vars

--- vars.tf
+++ vars.tf
@@ -2,3 +2,13 @@
       
 }*/
 #variable "name" {}
+
+variable "instance_type" {
+    type = map(string)
+    default = {
+      "default" = "t2.micro"
+      "DEV" = "t2.small"
+      "prod" = "t2.medium"
+    }
+  
+}
