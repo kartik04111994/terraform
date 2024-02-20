3a62852d749b0e685f59e4b2a3011d9fb0cf072e
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-20 19:30:30 +0530

    pushing backend config

---
+++ s3-bucket.tf
@@ -0,0 +1,22 @@
+/*resource "aws_s3_bucket" "demo-bucket" {
+    bucket = "terraform-bucket-demo"
+    tags = {
+      name = "terraform-bucket-demo"
+      
+    }
+  
+}*/
+
+#############################################
+// S3 backend for storing state files
+##################################
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-demo"
+    key    = "Dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
