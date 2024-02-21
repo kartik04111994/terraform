988dfcc88f08aec64bbefcc4224faedc9b82c85a
kartik04111994 <110240969+kartik04111994@users.noreply.github.com>
2024-02-21 12:46:27 +0530

    pushing code for s3 backend

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,25 +2,53 @@
 // S3 resource creation
 ##################################
 
-/*resource "aws_s3_bucket" "demo-bucket" {
+resource "aws_s3_bucket" "demo-bucket" {
     bucket = "terraform-bucket-demo"
     tags = {
-      name = "terraform-bucket-demo"
-      
+      name = "terraform-bucket-demo"      
     }
-  
-}*/
+}
+resource "aws_s3_object" "Dev" {
+  bucket = aws_s3_bucket.demo-bucket.bucket
+  key    = "Dev/"
+  acl    = "private"
+}
+resource "aws_s3_object" "Prod" {
+  bucket = aws_s3_bucket.demo-bucket.bucket
+  key    = "Prod/"
+  acl    = "private"
+}
+
+##########################################
+// DYnamo DB table creations for acquiring STATE_ LOCKING 
+###########################################
+resource "aws_dynamodb_table" "lockid_table" {
+  name           = "stateLocktable"
+  hash_key       = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"  # S for Strings
+  }
+
+  tags = {
+    Name = "stateLocktable"
+  }
+}
 
 #############################################
 // S3 backend for storing state files
 ##################################
-
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-demo"
     key    = "Dev/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "stateLocktable"  # newly created dynamoDB table 
   }
 }
+
+
+
 
 
