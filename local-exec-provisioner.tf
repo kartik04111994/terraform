452b8214154ba825a6c97ed04fc431360cd3c1a5
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-21 18:23:09 +0530

    commented code

--- local-exec-provisioner.tf
+++ local-exec-provisioner.tf
@@ -1,8 +1,13 @@
 /*resource "aws_instance" "myec2" {
    ami = "ami-0e731c8a588258d0d"
    instance_type = "t2.micro"
+   tags = {
+     name = "linux"
+     Name = "demo-linux"
+   }
 
    provisioner "local-exec" {
     command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
   }
-}*/
+}
+*/
