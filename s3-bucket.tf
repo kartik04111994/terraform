9e7dfcbd21e59ecaf80f2c5982063944c867cf38
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-02-21 20:31:32 +0530

    pushing updated code with force destroy option

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,22 +2,25 @@
 // S3 resource creation
 ##################################
 
-/*resource "aws_s3_bucket" "demo-bucket" {
+resource "aws_s3_bucket" "demo-bucket" {
     bucket = "terraform-bucket-demo"
+    force_destroy = true
     tags = {
       name = "terraform-bucket-demo"      
     }
 }
 resource "aws_s3_object" "Dev" {
   bucket = aws_s3_bucket.demo-bucket.bucket
-  key    = "Dev/"
+  key    = "Eip/"
   acl    = "private"
+  force_destroy = true
 }
 resource "aws_s3_object" "Prod" {
   bucket = aws_s3_bucket.demo-bucket.bucket
-  key    = "Prod/"
+  key    = "SG/"
   acl    = "private"
-} */
+  force_destroy = true
+}
 
 
 
