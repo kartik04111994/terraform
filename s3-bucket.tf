d50bf2311494ef6df2bdb4794c0387d9e9c2778e
kartik04111994 <110240969+kartik04111994@users.noreply.github.com>
2024-02-21 15:55:26 +0530

    pushing updates

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,7 @@
 #############################################
 // S3 resource creation
 ##################################
-##########
-// subfolders under the bucket Dev and Prod
-##################
+
 resource "aws_s3_bucket" "demo-bucket" {
     bucket = "terraform-bucket-demo"
     tags = {
@@ -21,34 +19,6 @@ resource "aws_s3_object" "Prod" {
   acl    = "private"
 }
 
-##########################################
-// DYnamo DB table creations for acquiring STATE_ LOCKING 
-###########################################
-resource "aws_dynamodb_table" "lockid_table" {
-  name           = "stateLocktable"
-  hash_key       = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"  # S for Strings
-  }
-
-  tags = {
-    Name = "stateLocktable"
-  }
-}
-
-#############################################
-// S3 backend for storing state files
-##################################
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-demo"
-    key    = "Dev/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "stateLocktable"  # newly created dynamoDB table 
-  }
-}
 
 
 
