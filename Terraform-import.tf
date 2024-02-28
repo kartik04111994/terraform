d70a8b14fea0c6a016827f4f2a9c1713d1502e10
kartik04111994 <110240969+kartik04111994@users.noreply.github.com>
2024-02-28 21:24:27 +0530

    Update Terraform-import.tf

--- Terraform-import.tf
+++ Terraform-import.tf
@@ -1,11 +1,11 @@
-provider "aws" {
+/*provider "aws" {
   region     = "us-east-1"
 }
 
 import {
   to = aws_security_group.mysg
   id = "sg-07f13feb262ba8b6f"
-}
+}*/
 
 ##Command To Autogenerate Code for Imported Resource
 
