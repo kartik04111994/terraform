ad6f4f320737979346a46270be67eee473f78c6f
kartik04111994 <shivkarthik.nov04@gmail.com>
2024-03-01 16:27:17 +0530

    pushing updates

--- myec2.tf
+++ myec2.tf
@@ -1,7 +1,8 @@
 provider "aws" {
     region = "us-east-1" 
 }
-resource "aws_instance" "democloud" {
+
+/*resource "aws_instance" "democloud" {
   ami = "ami-0e731c8a588258d0d"
   instance_type = "t2.micro"
 
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-#istest = true
-#name=[ "demo1", "demo2" ]
-#prod=[ "prod-instance1", "prod-instance2" ]
-#dev=[ "dev-instance1", "dev-instance2" ]
